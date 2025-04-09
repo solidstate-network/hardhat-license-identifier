5bd978713124acc5029dace149c1a58e8007063c
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-09 17:25:23 -0600

    register plugin in local hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,12 @@
-import './src/index';
+import HardhatSpdxLicenseIdentifier from './src/index.js';
+import type { HardhatUserConfig } from 'hardhat/config';
 
 export default {
   solidity: {
     version: '0.8.25',
   },
+};
+
+const config: HardhatUserConfig = {
+  plugins: [HardhatSpdxLicenseIdentifier],
 };
