359a0f32e989f8707cb7863a5bdc4f10c1c5284a
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-09 15:18:19 -0600

    export HardhatPlugin object

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import pkg from '../package.json';
 import './tasks/compile';
 import './tasks/prepend_spdx_license';
 import './type-extensions.js';
+import { HardhatPlugin } from 'hardhat/types/plugins';
 
 // import { extendConfig } from 'hardhat/config';
 
@@ -15,3 +17,10 @@ import './type-extensions.js';
 //     userConfig.spdxLicenseIdentifier,
 //   );
 // });
+
+const plugin: HardhatPlugin = {
+  id: pkg.name.split('/').pop()!,
+  npmPackage: pkg.name!,
+};
+
+export default plugin;
