57de80f3888dc89114221534dabf37b3f72a64d3
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-10 09:22:12 -0600

    fix config export

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,8 @@
 import HardhatSpdxLicenseIdentifier from './src/index.js';
 import type { HardhatUserConfig } from 'hardhat/config';
 
-export default {
-  solidity: {
-    version: '0.8.25',
-  },
-};
-
 const config: HardhatUserConfig = {
   plugins: [HardhatSpdxLicenseIdentifier],
 };
+
+export default config;
