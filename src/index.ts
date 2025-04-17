904235289ad3e3740566399afa9cd2e6d3b04479
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-16 19:45:22 -0600

    fix case of type_extensions file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import pkg from '../package.json';
 import compileTask from './tasks/compile.js';
 import prependLicenseTask from './tasks/prepend_license.js';
-import './type-extensions.js';
+import './type_extensions.js';
 import { HardhatPlugin } from 'hardhat/types/plugins';
 
 const plugin: HardhatPlugin = {
