31a7dd3375ce12df638ed3529090f1152fb6fed3
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-20 14:18:37 -0600

    remove hardhat-node-test-runner

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,8 @@
 import HardhatLicenseIdentifier from './src/index.js';
-import HardhatNodeTestRunner from '@nomicfoundation/hardhat-node-test-runner';
 import type { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
-  plugins: [HardhatNodeTestRunner, HardhatLicenseIdentifier],
+  plugins: [HardhatLicenseIdentifier],
 };
 
 export default config;
