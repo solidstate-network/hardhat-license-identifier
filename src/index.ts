ca3290ad5a6ebbaef735fbaf788e8e8793aca439
Nick Barry <itsnickbarry@protonmail.ch>
2026-06-07 16:22:06 +0900

    wrap plugin export with hardhat definePlugin

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import taskLicense from './tasks/license.js';
 import taskLicenseList from './tasks/license_list.js';
 import taskLicenseWrite from './tasks/license_write.js';
 import './type_extensions.js';
+import { definePlugin } from 'hardhat/plugins';
 import { HardhatPlugin } from 'hardhat/types/plugins';
 
-const plugin: HardhatPlugin = {
+const plugin: HardhatPlugin = definePlugin({
   id: pkg.name!,
   npmPackage: pkg.name!,
   dependencies: () => [import('@solidstate/hardhat-solidstate-utils')],
@@ -14,6 +15,6 @@ const plugin: HardhatPlugin = {
     config: () => import('./hooks/config.js'),
     solidity: () => import('./hooks/solidity.js'),
   },
-};
+});
 
 export default plugin;
